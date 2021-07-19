693a9324afdf08f7f68d79acc92c1462b2259511
Eugen Lukashuk <lukashuk2510@gmail.com>
2021-07-19 10:55:13 +0300

    fix after create remote

---
+++ Test/Extensions/UIImageView.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageView.swift
+//  Test
+//
+//  Created by Eugen on 19.07.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImageFromURL(url: String){
+        if let url = NSURL(string: url) {
+            if let data = NSData(contentsOf: url as URL) {
+                self.image = UIImage(data: data as Data)
+            }
+        }
+    }
+}
+
